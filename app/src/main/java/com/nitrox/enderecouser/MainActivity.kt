b1547b010cc7c75d12ef106ea2bfd5986c9fe8fe
alelima <alessandrolima@gmail.com>
2019-04-22 17:32:49 -0300

    Aplicação já com um content resolver lendo informações do cursor apontando para o content provider da aplicação EnderecoEX

---
+++ app/src/main/java/com/nitrox/enderecouser/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.nitrox.enderecouser
+
+import android.net.Uri
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import org.jetbrains.annotations.Contract
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        object: AsyncTask<Void, Void, Unit>() {
+            override fun doInBackground(vararg params: Void?) {
+                val selectionClause = "id" + " = ?"
+                val selectionArgs = arrayOf("3")
+                val cursor = contentResolver.query(
+                    Uri.parse("content://com.nitrox.enderecoex.provider/endereco"), null, selectionClause, selectionArgs, null
+                )
+                //cursor.moveToFirst();
+                cursor.move(1)
+                Log.v(TAG, "Cursor da consulta: ${cursor.getString(0)}")
+            }
+        }.execute()
+
+
+
+    }
+}
